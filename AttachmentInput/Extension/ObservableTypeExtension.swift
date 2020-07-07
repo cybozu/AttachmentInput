da605b4c838512d0bc1b2f8a8db3084703c3c9c2
daikimat <daiki-matsumoto@cybozu.co.jp>
2020-07-07 15:08:50 +0900

    remove unnecessary public protocol and method

--- AttachmentInput/Extension/ObservableTypeExtension.swift
+++ AttachmentInput/Extension/ObservableTypeExtension.swift
@@ -9,7 +9,7 @@
 import Foundation
 import RxSwift
 
-public protocol Optionable {
+protocol Optionable {
     associatedtype Wrapped
     func flatMap<U>(_ transform: (Wrapped) throws -> U?) rethrows -> U?
     func map<U>(_ transform: (Wrapped) throws -> U) rethrows -> U?
@@ -22,7 +22,7 @@ extension ObservableType where Element: Optionable {
     Takes a sequence of optional elements and returns a sequence of non-optional elements, filtering out any nil values.
     - returns: An observable sequence of non-optional elements
     */
-    public func unwrap() -> Observable<Element.Wrapped> {
+    func unwrap() -> Observable<Element.Wrapped> {
         return self
             .filter { $0.map { $0 } != nil } // filter{ Type? != nil }
             .map { ($0.map { $0 })! } // map{ (Type?)! }
